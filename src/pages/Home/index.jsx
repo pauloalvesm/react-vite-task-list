905efdda295665c5dff5c1d7d746503ed5a8b6e4
pauloalvesm <paulo.alves7351@gmail.com>
2025-10-02 20:51:49 -0300

    feat: add elements to the home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,51 @@
+import { useState } from "react";
+import "./home.css";
+
+import { Link } from "react-router-dom";
+
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    if (email !== "" && password !== "") {
+      alert("Test");
+    } else {
+      alert("Fill in all the fields!");
+    }
+
+  }
+
   return (
-    <>
-    <div>
+    <div className="home-container">
       <h1>Task List</h1>
+      <span>Manage your schedule easily.</span>
+
+      <form className="form" onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Enter your email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          autoComplete="off"
+          type="password"
+          placeholder="******"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Access</button>
+      </form>
+
+      <Link className="button-link" to="/register">
+        Don't have an account? Sign up
+      </Link>
+
     </div>
-    </>
   )
 }
