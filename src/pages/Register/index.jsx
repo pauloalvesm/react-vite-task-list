81466b3314fdfc76a25af41c5d9d8f5e4ef45a52
pauloalvesm <paulo.alves7351@gmail.com>
2025-10-13 20:43:34 -0300

    feat: add visual elements to the register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
-
 import { Link } from "react-router-dom";
 import { auth } from "../../services/firebaseConnection";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { FaUserPlus } from "react-icons/fa6";
+import { FaSave } from "react-icons/fa";
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const [isPulsing, setIsPulsing] = useState(false);
 
   async function handleRegister(e) {
     e.preventDefault();
+
+    setIsPulsing(true);
 
     if (email.trim() !== "" && password.trim() !== "") {
       await createUserWithEmailAndPassword(auth, email, password)
@@ -29,7 +33,10 @@ export default function Register() {
 
   return (
     <div className="home-container">
-      <h1>Register</h1>
+      <h1 className="title-container">
+        <FaUserPlus className="icon-title" /> Register
+      </h1>
+
       <span>Let"s create your account!</span>
 
       <form className="form" onSubmit={handleRegister}>
@@ -47,7 +54,11 @@ export default function Register() {
           onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button type="submit" >Register</button>
+        <button type="submit" className={isPulsing ? "animation-pulse-1s" : ""} >
+          <span className="button-content">
+            Register <FaSave className="icon-button" />
+          </span>
+        </button>
       </form>
 
       <Link className="button-link" to="/">
